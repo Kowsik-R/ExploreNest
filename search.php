b7993595d9aa33bfbff2b8bd98efe4cfd3f0771f
Kowsik-R <kowsik.roy@g.bracu.ac.bd>
2024-09-25 11:39:13 +0600

    Update search.php
    sorting added

--- search.php
+++ search.php
@@ -6,9 +6,6 @@
     <title>Search the khoj</title>
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
-    
-
 </head>
 <body>
 
@@ -36,7 +33,7 @@
     font-size: 18px;
     font-weight: bold;
 }
-.btn {
+.btn {`
     padding: 10px 20px;
     background-color: #007bff;
     color: #fff;
@@ -48,25 +45,50 @@
 }
 </style>
 
-
 <?php
 include './design/header.php'; 
 include 'DBconnect.php';
+
+// Handling the sorting option order by##kowsik
+$sort_order = "";
+if (isset($_GET['sort'])) {
+    $sort_option = $_GET['sort'];
+    if ($sort_option == "low_high") {
+        $sort_order = "ORDER BY product_price ASC";
+    } elseif ($sort_option == "high_low") {
+        $sort_order = "ORDER BY product_price DESC";
+    }
+}
 
 if (isset($_GET['cs'])) {
 
     $q = $_GET['query'];
     $user_id = $_GET['id'];
 
-    $sql = "SELECT * FROM package WHERE product_name LIKE '%$q%';";
+    // Modified SQL query to include sorting ###KOWSIK
+    $sql = "SELECT * FROM package WHERE product_name LIKE '%$q%' $sort_order;";
     $result = mysqli_query($connection, $sql);
-
-    //  result
-    while ($row = mysqli_fetch_assoc($result)):
 ?>
+
+<!-- Form for sorting -->
+<div class="container">
+    <form action="" method="get" class="form-inline">
+        <input type="hidden" name="cs" value="1">
+        <input type="hidden" name="query" value="<?= htmlspecialchars($q) ?>">
+        <input type="hidden" name="id" value="<?= htmlspecialchars($user_id) ?>">
+        
+        <label for="sort" class="mr-2">Sort by Price:</label>
+        <select name="sort" id="sort" class="form-control" onchange="this.form.submit()">
+            <option value="">Select</option>
+            <option value="low_high" <?= (isset($sort_option) && $sort_option == 'low_high') ? 'selected' : '' ?>>Low to High</option>
+            <option value="high_low" <?= (isset($sort_option) && $sort_option == 'high_low') ? 'selected' : '' ?>>High to Low</option>
+        </select>
+    </form>
+</div>
 
 <section class="activities">
   <div class="box-container">
+    <?php while ($row = mysqli_fetch_assoc($result)): ?>
     <div class="box">
       <div class="image">
         <img src="<?= $row['product_image'] ?>" alt="">
@@ -87,15 +109,12 @@ if (isset($_GET['cs'])) {
         </form>
       </div>
     </div>
+    <?php endwhile; ?>
   </div>
 </section>
 
-<?php endwhile; } ?>
+<?php } ?>
 
-<?php
-
-include './design/footer.php'; 
-?>
-
+<?php include './design/footer.php'; ?>
 </body>
 </html>
